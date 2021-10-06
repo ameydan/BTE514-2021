acbc8ecb046a726459f0397ded4dd420b9e7b7ef
Tolga Ovatman <tolga.ovatman@icrontech.com>
2021-10-06 21:20:13 +0300

    first week examples

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello World!!!')
+import math
+
+# print("Hello", "World", "!!!", sep="--", end=">>>>")
+# age = input("Your age: ")
+a = 30
+n = "Tolga"
+s = "Ovatman"
+mystr = "Hello Mr.{surname}, your age is {age}"
+print(mystr.format(age = a, surname = s))
